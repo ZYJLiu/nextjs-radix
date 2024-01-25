cdf514158b416204ecd131f622d1bc90eb8bf234
John <johnzhengyuliu@gmail.com>
2024-01-24 20:08:22 -0600

    stripe error

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,18 +1,16 @@
 "use client";
 import { createClient } from "@/utils/supabase-auth/client";
-import { getURL } from "@/utils/helpers";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 
 export default async function SignIn() {
-  console.log(getURL());
   const supabase = createClient();
   return (
     <div className="flex flex-col space-y-4">
       <Auth
         supabaseClient={supabase}
         providers={["github"]}
-        redirectTo={`${getURL()}/auth/callback`}
+        redirectTo={`${window.location.origin}/auth/callback`}
         magicLink={false}
         appearance={{
           theme: ThemeSupa,
