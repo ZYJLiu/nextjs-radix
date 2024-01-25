d68f4dd8b7947b7726fcf1b36ae99df945f2cc11
John <johnzhengyuliu@gmail.com>
2024-01-24 20:10:19 -0600

    window not defined error

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,16 +1,18 @@
 "use client";
 import { createClient } from "@/utils/supabase-auth/client";
+import { getURL } from "@/utils/helpers";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 
 export default async function SignIn() {
+  console.log(getURL());
   const supabase = createClient();
   return (
     <div className="flex flex-col space-y-4">
       <Auth
         supabaseClient={supabase}
         providers={["github"]}
-        redirectTo={`${window.location.origin}/auth/callback`}
+        redirectTo={`${getURL()}/auth/callback`}
         magicLink={false}
         appearance={{
           theme: ThemeSupa,
